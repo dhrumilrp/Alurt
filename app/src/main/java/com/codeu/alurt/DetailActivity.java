0cf4c9d9d049554f46d3c0c6b09c4d2aa190b998
dhrumilrp <dhrumil.r.patel95@gmail.com>
2015-08-11 18:50:14 -0400

    panic button active

--- app/src/main/java/com/codeu/alurt/DetailActivity.java
+++ app/src/main/java/com/codeu/alurt/DetailActivity.java
@@ -1,15 +1,24 @@
 package com.codeu.alurt;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.widget.Toast;
+
+import java.util.List;
 
 
-public class DetailActivity extends ActionBarActivity {
+public class DetailActivity extends ActionBarActivity implements LocationListener {
+
+    boolean gps_enabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +30,57 @@ public class DetailActivity extends ActionBarActivity {
                     .add(R.id.container, new DetailActivityFragment())
                     .commit();
         }
+
+        curLocation = getLastKnownLocation();
+
+    }
+
+    Location curLocation = new Location("");
+
+
+    public Location getLastKnownLocation(){
+        LocationManager mLocationManager = (LocationManager)getApplicationContext().getSystemService(LOCATION_SERVICE);
+
+        gps_enabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if(gps_enabled) Toast.makeText(this, "location not null", Toast.LENGTH_SHORT).show();
+
+        List<String> providers = mLocationManager.getProviders(true);
+        Location bestLocation = null;
+        for (String provider : providers) {
+            Location l = mLocationManager.getLastKnownLocation(provider);
+            if (l == null) {
+                continue;
+            }
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                bestLocation = l;
+            }
+            Log.d("Provider", provider);
+            Log.d("Accuracy", Float.toString(l.getAccuracy()));
+        }
+
+        return bestLocation;
     }
 
 
+
+    @Override
+    public void onLocationChanged(Location loc) {
+        Log.d("Updated location", loc.toString());
+        curLocation = loc;
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {}
+
+    @Override
+    public void onProviderEnabled(String provider) {}
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+
     public void panicAlert(View view){
-        startActivity(DetailActivityFragment.createShareAlertIntent());
+        startActivity(DetailActivityFragment.createShareAlertIntent(curLocation));
     }
 
 

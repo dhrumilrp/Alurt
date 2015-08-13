229510539ae9fbfeb335fe7866b919ab5707adb7
dhrumilrp <dhrumil.r.patel95@gmail.com>
2015-08-13 15:20:06 -0700

    Tablet two pane UI set up

--- app/src/main/java/com/codeu/alurt/MainActivity.java
+++ app/src/main/java/com/codeu/alurt/MainActivity.java
@@ -10,18 +10,44 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private boolean mTwoPane;
+    private String DETAIL_ACTIVITY_FRAGMENT_TAG = "DAFTAG";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new MainActivityFragment())
-                    .commit();
+
+
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.container, new MainActivityFragment())
+//                    .commit();
+//        }
+
+        if (findViewById(R.id.emergency_detail_container) != null) {
+            // The detail container view will be present only in the large-screen layouts
+            // (res/layout-sw600dp). If this view is present, then the activity should be
+            // in two-pane mode.
+
+            Toast.makeText(this, "Screen size changed", Toast.LENGTH_SHORT).show();
+            mTwoPane = true;
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.emergency_detail_container, new DetailActivityFragment(), DETAIL_ACTIVITY_FRAGMENT_TAG)
+                        .commit();
+            }
+        } else {
+            mTwoPane = false;
         }
     }
 
